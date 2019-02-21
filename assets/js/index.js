3b7d65f00e1cccb1e55eae7d7b0eaf617246a740
Martyn Stirk <startinmerc@gmail.com>
2019-02-21 18:13:26 +0000

    big refactor of listeners, and making (nearly) all animations JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,11 @@
-const $menuTitle = $("#menuTitle"),
+var $menuTitle = $("#menuTitle"),
 	$menuLis = Array.from($(".menuItem")),
 	$menu = $('.menu:first');
 
-var listPop = anime({
-	targets: $menuLis,
-	translateX: ["-200%","0%"],
-	delay: anime.stagger(200, {start: 300}),
-	autoplay: false
-});
+var	isTop = false,
+	expanded = false;
+
+
 
 var titleTextOptions = {
 	strings: ["JAM_RACKET","JACKET_ARM","JAM_RACKET","TACKER_JAM","JAM_RACKET","EAT_MR_JACK","JAM_RACKET","CAT_AM_JERK","JAM_RACKET"],
@@ -16,15 +14,46 @@ var titleTextOptions = {
 
 var titleTextTyped = new Typed("#titleText", titleTextOptions);
 
+var menuExpand = anime.timeline({
+	autoplay: false,
+	complete: function(){
+		menuExpand.reverse();
+	}
+});
+
+menuExpand.add({
+	targets: '.menuItem',
+	translateX: ["-200%","0%"],
+	delay: anime.stagger(50, {start: 50}),
+	easing: 'easeOutElastic',
+})
+.add({
+	targets: '.menu',
+	height: ["77px","410px"],
+	duration: 200,
+},0);
+
+
+var menuToTop = anime({
+	targets: '.menu',
+	top: ['50%','0%'],
+	left: ['50%','0'],
+	translateY: ['-50%','0%'],
+	translateX: ['-50%','0%'],
+	boxShadow: [{value: ['10px 10px 0 0 currentColor','0px 10px 0 0 currentColor']}],
+	width: ['70vw','90vw'],
+	autoplay: false,
+	complete: function(){
+		isTop = true;
+	},
+	duration: 200
+});
+
 
 $(document).ready(function(){
 	addMenuListeners();
 })
 
-function menuToTop() {
-	$menu.removeClass("menuActive");
-	$menu.addClass("menuTop");
-};
 
 function updateText(i) {
 	$("#green .text").html(greenContent[i]);
@@ -33,12 +62,61 @@ function updateText(i) {
 };
 
 function addMenuListeners() {
-	$menuLis.forEach(function(v,i,a){
-		v.addEventListener("click", menuToTop, {once: true});
-		v.addEventListener("click", function(){updateText(i)}, false);
-	});
-	$menuTitle.click(function(){
-		$menu.toggleClass("menuActive");
-	});
-	document.querySelector("#menuTitle").onclick = listPop.play;
+	// Call add event listeners for menu items
+	$menuLis.forEach((v,i) => addMenuItemListener(v,i));
+	// Call add event listener for menu title
+	addMenuTitleListener();
 };
+
+function addMenuItemListener(v,i){
+	// Add click event listener to menu item
+	v.addEventListener("click", function(){
+		console.log(expanded);
+		// Call updateText for related index in content.js
+		updateText(i);
+		// Minimise menu
+		menuExpand.play();
+		// expanded = false
+		expanded = false;
+		// If isTop is false, i.e if menu is centre screen
+		if (!isTop) {
+			// Play animation to move menu to top
+			menuToTop.play();			
+		};
+		event.stopPropagation();
+	});
+}
+
+function addMenuTitleListener(){
+	// Add to menu title
+	document.querySelector(".menu").addEventListener("click", function(){
+		console.log(expanded);
+		// if menu is expanded
+		if (expanded) {
+			// play menu expansion timeline in reverse
+			menuExpand.play();
+			// update expanded
+			expanded = false;
+		// otherwise
+		} else {
+			// play menu expansion
+			menuExpand.play();
+			// update expanded
+			expanded = true;
+		};
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
